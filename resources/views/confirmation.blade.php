19110217fb09aed02d6b31b261587b9a6ae02020
Amaël Rosales <137775508+Rosales-Amael@users.noreply.github.com>
2025-12-12 21:33:46 +0100

    (feat): update page title in confirmation view

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transfert réussi</title>
+    <title>GreenTransfer</title>
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <link rel="alternate icon" href="{{ asset('favicon.ico') }}">
     <style>
